22f34f62c8855ce86a462cdc96ce6a9d9f9364c7
FreeBird <lizhuohang237@gmail.com>
2023-10-24 22:14:14 +0800

    add comment plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,7 +1,8 @@
 return {
 	{
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4', dependencies = { 'nvim-lua/plenary.nvim' },
+		tag = '0.1.4',
+		dependencies = { 'nvim-lua/plenary.nvim' },
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files" },
 			{ "<leader>fo", "<cmd>Telescope oldfiles<cr>",   desc = "find oldfiles" },
@@ -41,4 +42,23 @@ return {
 			})
 		end,
 	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring() or
+					vim.bo.commentstring
+				end,
+			},
+		},
+	}
 }
