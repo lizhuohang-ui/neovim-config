a678700bd07405eb47383d55bdfbcb12843e2dd7
Zhuohang Li <lizhuohang237@gmail.com>
2026-05-04 23:11:25 +0800

    fix: fix treeistter version bug

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,60 @@
+local install_dir = vim.fn.stdpath("data") .. "/site"
+local parsers = {
+	"c",
+	"cpp",
+	"lua",
+	"vim",
+	"vimdoc",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"bash",
+	"json",
+	"query",
+}
+local filetypes = {
+	"c",
+	"cpp",
+	"lua",
+	"vim",
+	"help",
+	"markdown",
+	"python",
+	"sh",
+	"bash",
+	"json",
+	"jsonc",
+}
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+		branch = "main",
+		lazy = false,
+		build = function()
+			local treesitter = require("nvim-treesitter")
 
-			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "python", "cpp" },
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
+			treesitter.setup({ install_dir = install_dir })
+			treesitter.install(parsers):wait(300000)
+			treesitter.update(parsers):wait(300000)
+		end,
+		config = function()
+			local treesitter = require("nvim-treesitter")
+
+			treesitter.setup({
+				install_dir = install_dir,
+			})
+
+			vim.treesitter.language.register("bash", { "sh", "bash" })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("UserTreesitter", { clear = true }),
+				pattern = filetypes,
+				callback = function()
+					if pcall(vim.treesitter.start) then
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
 			})
 		end
 	},
